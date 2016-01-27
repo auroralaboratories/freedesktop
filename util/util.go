b9c2e0be9ddf729070e2cf50eb086ee444bf7ad8
Gary Hetzel <garyhetzel@gmail.com>
2016-01-27 17:30:03 -0500

    Cleanup comments

--- util/util.go
+++ util/util.go
@@ -4,6 +4,7 @@ import (
     "os"
 )
 
+// Retrive a named environment variable or return a fallback value in one call
 func Getenv(name string, fallback string) string {
     if rv := os.Getenv(name); rv == `` {
         return fallback
@@ -12,6 +13,8 @@ func Getenv(name string, fallback string) string {
     }
 }
 
+
+// Attempt to open a given file read-only and close it to verify existence and readability
 func FileExistsAndIsReadable(name string) bool {
     file, err := os.Open(name)
     defer file.Close()
