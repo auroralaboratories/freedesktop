9b962e605edd2a5ad882fe4d313f86da1fe31b0d
Gary Hetzel <garyhetzel@gmail.com>
2016-01-27 17:34:45 -0500

    Cleanup comments

---
+++ desktop/desktop.go
@@ -0,0 +1,4 @@
+// The desktop entry specification describes desktop entries: files describing information about an application such as
+// the name, icon, and description. These files are used for application launchers and for creating menus of
+// applications that can be launched.
+package desktop
